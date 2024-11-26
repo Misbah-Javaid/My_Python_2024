ab758f9d8223975853620efa47fde82ed538d32b
Misbah Javaid <misbah.javaid2014@gmail.com>
2024-11-27 01:47:11 +1000

    This is first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import sys
+from os import rename
+import math
+import requests
+
+"""print(sys.version)
+print(sys.executable)
+print("This is not working and I am tring hard)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greet"""
+
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
+print("This is not working and I am tring hard")
