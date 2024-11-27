36e4ac41394c9fad8846ff6a5cf9c295192eeeae
Misbah Javaid <misbah.javaid2014@gmail.com>
2024-11-27 23:18:20 +1000

    Test 2

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ from os import rename
 import math
 import requests
 
-"""print(sys.version)
+print(sys.version)
 print(sys.executable)
-print("This is not working and I am tring hard)
+print("This is not working and I am tring hard")
 
 
 def greet(who_to_greet):
     greeting = "Hello, {}".format(who_to_greet)
-    return greet"""
+    return greet
 
 
 r = requests.get('https://coreyms.com')
