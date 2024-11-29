52f1c1abc69b67eb78b8f001fef9cef5fe4de839
Misbah Javaid <misbah.javaid2014@gmail.com>
2024-11-30 00:43:06 +1000

    while loop

---
+++ while.py
@@ -0,0 +1,19 @@
+spam = 0
+
+if spam < 5:
+    print("Hello Misbah")
+    spam = spam + 1
+    print(spam)
+    
+while spam <5:
+    print("This is while loop")
+    spam = spam + 1
+print(spam)    
+
+# This program will display your name
+
+name = ""
+
+while name != "Your name":
+    name = input("Please enter your name: ")
+print("Thank you")    
