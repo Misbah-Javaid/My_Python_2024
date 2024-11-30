482f46d1e43ce8c2611ff2c2ab1c1e5e91f51f5a
Misbah Javaid <misbah.javaid2014@gmail.com>
2024-12-01 00:46:51 +1000

    while and for for_loop

---
+++ for_loop.py
@@ -0,0 +1,80 @@
+"""for i in range(5):
+    print(i, "Misbah")
+
+# Add all numbers from 0 to 100
+
+total = 0
+
+for i in range(101):
+    total = total + i
+    print(i, total)
+    
+num = 0
+while num < 5:
+    print("Misbah") 
+    num += 1  
+    
+for i in range(5,-1, -1):
+    print(i)  
+    
+    
+# Generate five random numbers from a range of 1 to 10
+
+import random
+
+for i in range(5):
+    print(random.randint(1,10))
+    
+# How to use sys module
+
+import sys
+
+while True:
+    
+    user_input = input("Type exit to exit: ")
+    if user_input == 'exit':
+        print("Good Bye")
+        sys.exit() 
+        
+# Write a program that prints Hello if 1 is stored in spam, prints 
+# Howdy if 2 is stored in spam
+# prints Greetings ! if anything else is stored in spam
+
+
+spam = int(input())
+
+if spam == 1:
+    print("Hello")
+elif spam == 2:
+    print("Howdy")
+    
+else:
+    print("Greetings")   
+    
+for i in range(10):
+    print(i)     
+
+for i in range(0, 10):
+    print(i)   
+
+for i in range(0,10,1):
+    print("skip", i)   """     
+         
+# Print numbers from 1 to 10 using for and while loop
+
+for i in range(1, 11):
+    print(i)
+    
+
+ 
+num = 1
+ 
+while num < 11:
+     print(num)
+     num = num + 1     
+    
+     
+
+        
+        
+    
