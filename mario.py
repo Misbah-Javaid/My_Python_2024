19e6a3073e555aaeb88e1938856c75320b753ebd
Misbah Javaid <misbah.javaid2014@gmail.com>
2025-02-19 20:16:19 +1000

    v

---
+++ mario.py
@@ -0,0 +1,19 @@
+# print("????")
+"""
+n = int(input("Size: "))
+while n < 1:
+    n = int(input("Please enter a positive number: "))
+for i in range(n):
+    #print("###", end ="")  # This will print four # on the same line
+    for j in range(n):
+        print("#", end = "")
+    print("#")  # This will print four # on the same line
+    
+# print("????") """
+
+a = 1
+b = 3
+rounded = round(1/3, 2)
+
+print(round(1/3, 7
+            ))
